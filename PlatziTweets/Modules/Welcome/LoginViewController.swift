880002ad413987b918df5f46a93db713b6c35466
Carlos Mejia <carlosmejia083@gmail.com>
2020-01-21 12:10:36 -0500

    Clase 20 - Configuración inicio de sesión.

---
+++ PlatziTweets/Modules/Welcome/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  PlatziTweets
+//
+//  Created by Luis Carlos Mejia Garcia on 21/01/20.
+//  Copyright © 2020 Mejia Garcia. All rights reserved.
+//
+
+import UIKit
+import NotificationBannerSwift
+
+class LoginViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func loginButtonAction() {
+        performLogin()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        loginButton.layer.cornerRadius = 25
+    }
+    
+    private func performLogin() {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            NotificationBanner(title: "Error", subtitle: "Debes especificar un correo.", style: .warning).show()
+            
+            return
+        }
+    }
+}
---
+++ PlatziTweets/Modules/Welcome/RegisterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisterViewController.swift
+//  PlatziTweets
+//
+//  Created by Luis Carlos Mejia Garcia on 21/01/20.
+//  Copyright © 2020 Mejia Garcia. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    private func setupUI() {
+        registerButton.layer.cornerRadius = 25
+    }
+}
